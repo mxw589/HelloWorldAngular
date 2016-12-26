0e4ed3ae9cbdff42b94149c910262220bdc193b6
Mark Willett <markwillett92@gmail.com>
2016-12-26 20:42:12 +0000

    Mirroring changes to base project

---
+++ app/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Mark on 26/12/2016.
+ */
+var app = angular.module("myApp", []);
--- app/components/exampleComponent/ComponentController.js
+++ app/components/exampleComponent/ComponentController.js
@@ -1,6 +1,6 @@
 /**
  * Created by Mark on 26/12/2016.
  */
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('ComponentController', ['$scope', function($scope) {
     $scope.title = 'Top Sellers in Books';
 }]);
