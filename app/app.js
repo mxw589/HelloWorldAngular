ce218a517b439c940d5c2ec047b15b8eb251ecac
Mark Willett <markwillett92@gmail.com>
2016-12-28 13:19:17 +0000

    no message

--- app/app.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Mark on 26/12/2016.
- */
-var app = angular.module("myApp", []);
---
+++ app/app.module.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Mark on 26/12/2016.
+ */
+angular.module("schedulingApp", [
+    "tileGrid",
+    "ngRoute"
+]);
